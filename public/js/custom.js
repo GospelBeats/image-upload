1c9b0edd99585bd403d641fb6a244f7c41548341
Cory <cyb12345@comcast.net>
2019-11-25 16:48:52 -0500

    upload image test

---
+++ public/js/custom.js
@@ -0,0 +1,7 @@
+
+// window.onload = function () {
+//     var meetingID = document.getElementById('meetingID')
+//     if (meetingID) {
+//         meetingID.value = window.location.pathname.split("/")[2];
+//     }
+// }
